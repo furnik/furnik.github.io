d0d4bad58d07db24e780f62e92fbd0b74d3d0b59
Serhii Furnik <furnik267@gmail.com>
2021-02-24 00:56:26 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const colorFig = document.querySelector('.color');
+const typeFig = document.querySelector('.type');
+const figure = document.querySelector('.figure');
+
+colorFig.addEventListener('change', onColorFig);
+typeFig.addEventListener('click', onTypeFig)
+
+function onTypeFig() {
+    figure.classList.remove('circle', 'rectangle', 'squire');
+    figure.classList.add(typeFig.value);
+};
+
+function onColorFig() {
+    figure.style.background = colorFig.value;
+};
